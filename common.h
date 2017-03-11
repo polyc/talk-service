d708b4158723b84ac5831aac0c7c45106adedb98
regibald <martinelli.fulco@gmail.com>
2017-03-11 17:54:33 +0100

    generic error helper

--- common.h
+++ common.h
@@ -9,11 +9,13 @@
 #include <unistd.h>
 
 
+#define SERVER_IP "...." //add IP address of the server   
+
 #define GENERIC_ERROR_HELPER(cond, errCode, msg) do {
         if (cond) {
             fprintf(stderr, "%s: %s\n", msg, strerror(errCode));
             exit(EXIT_FAILURE);
-        }                                                           
+        }
     } while(0)
 
 #define ERROR_HELPER(ret, msg)          GENERIC_ERROR_HELPER((ret < 0), errno, msg)
