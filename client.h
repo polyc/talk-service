89783debd91029ab8016c71d480ac60e569d8d82
polyc <ichigo69600@gmail.com>
2017-03-13 19:24:42 +0100

    removed unused data structure

--- client.h
+++ client.h
@@ -2,14 +2,6 @@
 
 #define CLIENT_IP "..." //add client IP address
 
-
-//user list typical element
-typedef struct usr_list_client_elem_s{
-  char[16] user_name;
-  struct listen_thread_t listen_thread;
-  char a_flag;
-}usr_list_client_elem_t;
-
 typedef struct listen_thread_args_s{
   int socket;
   struct sockaddr_in* addr;
