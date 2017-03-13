715525545f02916811344009da4b2e63031997d2
regibald <martinelli.fulco@gmail.com>
2017-03-13 11:46:47 +0100

    Merge branch 'dev' of https://github.com/polyc/talk-service into dev
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- server.h
+++ server.h
@@ -1,5 +1,13 @@
 #include "common.h"
 
 #define MAXUSERS 10
+#define SERVER_QUIT "QUIT"
 
 void* client-process/server-thread_connection_handler(void* arg);
+
+//data structure passed to threads on creation
+typedef struct thread_args_s{
+  int socket;
+  unsigned int thread_id;
+  struct sockaddr_in* addr;
+} thread_args_t;
