29ba5d53b6f9eefb2cf3b6be2d54a147591c428a
regibald <martinelli.fulco@gmail.com>
2017-03-13 17:06:06 +0100

    added listen_thread_routine and usr_list_recv_thread_routine(not finished). added code where the client sends the username to the server

--- client.c
+++ client.c
@@ -11,50 +11,96 @@
 
 #include "common.h"
 
+
+
+//thread listen routine
 void* listen_thread_routine(void *args){
+
   struct listen_thread_args_t* arg = args;
+
   int socket = arg.socket;
+  struct sockaddr_in* address = arg.addr;
+
+  //binding listen thread address to listen thread socket
+  ret = bind(socket_listen_thread_desc, (const struct sockaddr*)&address, sizeof(struct sockaddr_in));
+  ERROR_HELPER(ret, "Error while binding address to listen thread socket");
 
   //thread_listen listening for incoming connections
   ret = listen(socket, 1);
   ERROR_HELPER(ret, "Cannot listen on listen_thread socket");
+
+  /*
+  *
+  *
+  *  P2P chat implementation
+  *
+  *
+  *
+  */
+
 }
 
+void* usr_list_recv_thread_routine(void *args){
+
+  struct listen_thread_args_t* arg = args;
+
+  int socket = arg.socket;
+  struct sockaddr_in* address = arg.addr;
+
+  //binding user list receiver thread address to user list receiver thread socket
+  ret = bind(socket_listen_thread_desc, (const struct sockaddr*)&address, sizeof(struct sockaddr_in));
+  ERROR_HELPER(ret, "Error while binding address to user list receiver thread socket");
+
+  //user list receiver thread listening for incoming connections
+  ret = listen(socket, 1);
+  ERROR_HELPER(ret, "Cannot listen on user list receiver thread socket");
+
+  /*
+  *
+  *
+  *  not finished
+  *
+  *
+  *
+  */
+
+}
 
 int main(){
 
   int ret;
 
   //socket descriptor to connect to server
-  int socket_server_desc = 0;
-  socket_server_desc = socket(AF_INET, SOCK_STREAM, 0);
-  ERROR_HELPER(socket_server_desc, "error while creating client socket descriptor");
+  int socket_server_desc = socket(AF_INET, SOCK_STREAM, 0);
+  ERROR_HELPER(socket_server_desc, "Error while creating client socket descriptor");
 
   //data structure for the connection to the server
-  struct sockaddr_in serv_addr;
-  serv_addr.sin_len      = sizeof(sockaddr_in);
-  serv_addr.sin_family   = AF_INET;
-  serv_addr.sin_port     = htons(SERVER_PORT);
-  serv_addr.sin_addr     = SERVER_IP;
+  struct sockaddr_in serv_addr = {0};
+  serv_addr.sin_len            = sizeof(sockaddr_in);
+  serv_addr.sin_family         = AF_INET;
+  serv_addr.sin_port           = htons(SERVER_PORT);
+  serv_addr.sin_addr.s_addr    = inet_addr(SERVER_IP);
 
   //socket descriptor for listen thread
-  int socket_listen_thread_desc = 0;
-  socket_listen_thread_desc  = socket(AF_INET, SOCK_STREAM, 0);
-  ERROR_HELPER(socket_listen_thread_desc, "error while creating client listen socket descriptor");
+  int socket_listen_thread_desc = socket(AF_INET, SOCK_STREAM, 0);
+  ERROR_HELPER(socket_listen_thread_desc, "Error while creating client listen socket descriptor");
 
-  //data structure for listen thread
+  //socket descriptor for user list receiver thread
+  int usrl_recv_socket = socket(AF_INET, SOCK_STREAM, 0);
+  ERROR_HELPER(usrl_recv_socket, "Error while creating user list receiver thread socket descriptor");
+
+  //address structure for listen thread socket
   struct sockaddr_in listen_address = {0};
-  serv_addr.sin_len     = sizeof(sockaddr_in);
-  serv_addr.sin_family  = AF_INET;
-  serv_addr.sin_port    = htons(SERVER_PORT);
+  serv_addr.sin_len                 = sizeof(sockaddr_in);
+  serv_addr.sin_family              = AF_INET;
+  serv_addr.sin_port                = htons(CLIENT_THREAD_LISTEN_PORT);
 
-  ret = bind(socket_listen_thread_desc, (const struct sockaddr*)&listen_address, sizeof(struct sockaddr_in));
-  ERROR_HELPER(ret, "error while binding address to listen thread socket");
+  //address structure for user list receiver thread socket
+  struct sockaddr_in usrl_recv_address = {0};
+  usrl_recv_address.sin_len            = sizeof(sockaddr_in);
+  usrl_recv_address.sin_family         = AF_INET;
+  usrl_recv_address.sin_port           = htons(CLIENT_THREAD_RECEIVER_PORT);
 
-  //listen thread data to pass to server
-  struct listen_thread_t listen_thread_data;
-  listen_thread_data.socket  = socket_listen_thread_desc;
-  listen_thread_data.address = listen_address;
 
 
   //thread listen
@@ -62,36 +108,64 @@ int main(){
   //creating parameters for listen thread funtion
   listen_thread_args_t* t_listen_args = malloc(sizeof(listen_thread_args_t*));
   t_listen_args.socket = socket_listen_thread_desc;
+  t_listen_args.addr   = listen_address;
 
   //creating and spawning thread listen with parameters
   pthread_t thread_listen;
   ret = pthread_create(&thread_listen, NULL, listen_thread_routine, &t_listen_args);
-  PTHREAD_ERROR_HELPER(ret, "unable to create listen_thread");
+  PTHREAD_ERROR_HELPER(ret, "Unable to create listen_thread");
 
   //detatching from listen_thread
   ret = pthread_detach(thread_listen);
-  PTHREAD_ERROR_HELPER(ret, "unable to detatch from listen_thread");
+  PTHREAD_ERROR_HELPER(ret, "Unable to detatch from listen_thread");
   //
   //detached from thread listen
 
 
-  //temporary receiver thread data structure to pass to server
-  struct usr_list_receiver_thread_t receiver_thread_data;
-  receiver_thread_data.socket  = 0;
-  receiver_thread_data.address = NULL;
+  //user list receiver thread
+  //
+  //creating parameters for user list receiver thread funtion
+  listen_thread_args_t* usrl_recv_args = malloc(sizeof(listen_thread_args_t*));
+  t_listen_args.socket = usrl_recv_socket;
+  t_listen_args.addr   = usrl_recv_address;
 
-  //client data structure to send to server
-  struct usr_list_server_elem_t client_data;
-  client_data.user_name     = "regibald_94";
-  client_data.listen_thread = listen_thread_data;
-  client_data.recv_thread   = receiver_thread_data; //not initiated will create null for now
-  client_data.a_flag        = AVAILABLE;
+  //creating and spawning user list receiver thread with parameters
+  pthread_t thread_usrl_recv;
+  ret = pthread_create(&thread_usrl_recv, NULL, usr_list_recv_thread_routine, &usrl_recv_args);
+  PTHREAD_ERROR_HELPER(ret, "Unable to create user list receiver thread");
+
+  //detatching from user list receiver thread
+  ret = pthread_detach(thread_usrl_recv);
+  PTHREAD_ERROR_HELPER(ret, "Unable to detatch from user list receiver thread");
+  //
+  //detached from user list receiver thread
+
+
 
   //connection to server
   ret = connect(socket_server_desc, (const struct sockaddr)&serv_addr, sizeof(serv_addr));
-  ERROR_HELPER(ret, "error trying to connect to server");
+  ERROR_HELPER(ret, "Error trying to connect to server");
 
-  //sending init data to server
-  ret = send()
 
+  //sending buffer init data for user list
+  //creating buffer for username and availability flag
+  char username* = "regibald_94"; //temporary username
+  char username_buf[16];
+  strncpy(username_buf, username, 16);
+
+  //sending username to server
+  ret = send(socket_server_desc, username_buf, strlen(username_buf), 0);
+
+  //making sure all bytes have been sent
+  int bytes_left = strlen(msg_to_send);
+  int bytes_sent = 0;
+
+  while (bytes_left > 0) {
+      ret = send(socket_server_desc, username_buf + bytes_sent, bytes_left, 0);
+      if (ret == -1 && errno == EINTR) continue;
+      ERROR_HELPER(ret, "Error while sending username to server");
+
+      bytes_left -= ret;
+      bytes_sent += ret;
+    }
 }
--- client.h
+++ client.h
@@ -1,5 +1,8 @@
 #include common.h
 
+#define CLIENT_IP "..." //add client IP address
+
+
 //user list typical element
 typedef struct usr_list_client_elem_s{
   char[16] user_name;
@@ -10,4 +13,9 @@ typedef struct usr_list_client_elem_s{
 typedef struct listen_thread_args_s{
   int socket;
   struct sockaddr_in* addr;
-}listen_thread_args_t;
+}listen_thread_args_t;  //change name beacuse used both by lsiten_thread and usrl_recv_thread
+
+
+void* usr_list_recv_thread_routine(void *args);
+
+void* listen_thread_routine(void *args);
