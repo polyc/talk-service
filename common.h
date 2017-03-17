950cc4bb1c305c2056751a4b2fce8c648ea3863e
polyc <ichigo69600@gmail.com>
2017-03-17 15:27:54 +0100

    fixed macro

--- common.h
+++ common.h
@@ -12,7 +12,7 @@
 #include <unistd.h>
 
 #define SERVER_PORT 1024
-#define SERVER_IP "...." //add IP address of the server
+#define SERVER_IP "192.168.0.1" //add IP address of the server
 #define CLIENT_THREAD_LISTEN_PORT "1025"
 #define CLIENT_THREAD_RECEIVER_PORT "1026"
 #define USERLIST_BUFF_SIZE 50
@@ -21,11 +21,11 @@
 #define UNAVAILABLE 'u'
 #define QUIT        'q'
 
-#define GENERIC_ERROR_HELPER(cond, errCode, msg) do {
-        if (cond) {
-            fprintf(stderr, "%s: %s\n", msg, strerror(errCode));
-            exit(EXIT_FAILURE);
-        }
+#define GENERIC_ERROR_HELPER(cond, errCode, msg) do {             \
+        if (cond) {                                               \
+            fprintf(stderr, "%s: %s\n", msg, strerror(errCode));  \
+            exit(EXIT_FAILURE);                                   \
+        }                                                         \
     } while(0)
 
 #define ERROR_HELPER(ret, msg)          GENERIC_ERROR_HELPER((ret < 0), errno, msg)
@@ -34,7 +34,7 @@
 
 //user list sender/receiver thread's single slot buffer
 typedef struct user_list_buffer_s{
-  char[16] user_name;
+  char user_name[16];
   char* client_ip;
   short user_elem_pos;
   char a_flag;
@@ -44,7 +44,7 @@ typedef struct user_list_buffer_s{
 
 //user list typical element
 typedef struct usr_list_elem_s{
-  char[16] user_name;
+  char user_name[16];
   char* client_ip;
   char a_flag;
 }usr_list_elem_t;
