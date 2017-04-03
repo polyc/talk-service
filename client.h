50b7d4229d32d444fc2a5d4958a2637028586d21
polyc <ichigo69600@gmail.com>
2017-04-03 12:58:11 +0200

    removed redundant func definition

--- client.h
+++ client.h
@@ -7,8 +7,6 @@ typedef struct receiver_thread_args_s{
 
 void* usr_list_recv_thread_routine(void *args);
 void* listen_thread_routine(void *args);
-void send_msg(int socket, char *buf);
-int recv_msg(int socket, char *buf, size_t buf_len);
 void update_list(char* buf_userName, usr_list_elem_t* elem, char* mod_command);
 void parse_elem_list(const char* buf, usr_list_elem_t* elem, char* buf_userName, char* mod_command);
 int get_username(char* username);
