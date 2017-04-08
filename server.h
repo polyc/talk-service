05f7a98a1f9ffb31ba2f2ff3a9106d06ba741164
polyc <ichigo69600@gmail.com>
2017-04-08 18:01:10 +0200

    revised server functions now more efficient, but not completed yet

--- server.h
+++ server.h
@@ -9,7 +9,8 @@ typedef struct thread_args_s{
 } thread_args_t;
 
 typedef struct sender_thread_args_s{
-  sem_t* chandler_sender_sync;
+  sem_t* chandler_sender_sync;//sync between chandlers and senders
+  GAsyncQueue* mailbox; //cHandlers put their messages here
 }sender_thread_args_t;
 
 //void create_user_list_element(usr_list_elem_t* element, char* ip, thread_args_t* args, char* buf);
@@ -17,9 +18,10 @@ typedef struct sender_thread_args_s{
 void* connection_handler(void* arg);
 void* sender_routine(void* arg);
 void stringify_user_element(char* buf_out, usr_list_elem_t* elem, char* buf_username, char mod_command);
-void receive_and_execute_command(thread_args_t* args, char* buf_command);
+void receive_and_execute_command(thread_args_t* args, char* buf_command, usr_list_elem_t* element_to_update);
 void send_list_on_client_connection(gpointer key, gpointer value, gpointer user_data);
-void update_availability(char* username, char* buf_command);
+void update_availability(usr_list_elem_t* element_to_update, char* buf_command);
 void remove_entry(char* username);
-void push_entry();
+void push_entry(char*parsed_message, GAsyncQueue* queue);
 void pop_entry();
+void broadcast_message();
